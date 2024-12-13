a027a9d44ba238b37c76d9e77e2d38898198e630
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2024-12-13 14:22:00 -0300

    fixes #2: now it connects to an AVD automatically

--- android_kotlin.sh
+++ android_kotlin.sh
@@ -36,6 +36,76 @@ get_application_name() {
   app_id=$(grep -oP 'applicationId\s*=\s*"\K[^"]+' "$GRADLE_FILE")
 }
 
+emulator_path=""
+look_android_emulator() {
+  echo -e "\n${yellow}Looking for virtual devices (AVDs)...${reset}\n"
+
+  emulator_path=$(find ~ 2>/dev/null | grep "Android/Sdk/emulator/emulator$")
+}
+
+device_not_found() {
+  while true; do
+    echo -e -n "\nDo you want to start an emulator? (Y/n) "
+    read choice
+
+    if [[ $choice == "y" || $choice == "Y" || $choice == "" ]]; then
+      look_android_emulator
+
+      if [[ -z "$emulator_path" ]]; then
+        echo -e "${red}ERROR: no AVDS found!${reset}"
+        echo -e "consider installing the Android SDK and creating an AVD."
+        return
+      fi
+
+      local avds=($($emulator_path -list-avds | tail -n +2))
+      local avds_count=${#avds[@]}
+
+      if [[ "$avds_count" -eq "0" ]]; then
+        echo -e "${red}No devices found.${reset}"
+      elif [[ "$avds_count" -eq "1" ]]; then
+        echo -e "${yellow}Starting the emulator...${reset}\n"
+        $emulator_path -avd ${avds[0]} &
+      else
+        echo -e "${yellow}Multiple device emulators found. Select one:${reset}"
+
+        while true; do
+          for i in "${!avds[@]}"; do
+            echo "($((i + 1))) ${avds[$i]}"
+          done
+
+          echo -e "\n(q) Quit"
+          echo -n "--> "
+          read choice
+
+          # checks if the input is a number
+          if [[ "$choice" =~ ^[0-9]+$ ]]; then
+            if [ "$choice" -le "$avds_count" ]; then
+              echo -e "${yellow}Starting the emulator...${reset}"
+              $emulator_path -avd ${avds[$((choice - 1))]} &
+              break
+            fi
+          fi
+
+          if [[ $choice == "q" || $choice == "Q" || $choice == "exit" || $choice == "Exit" ]]; then
+            return
+          fi
+
+          clear
+          echo -e "${red}Invalid selection. Try again.${reset}\n"
+          echo -e "${yellow}Multiple emulators found. Select one:${reset}"
+        done
+      fi
+
+      break
+    elif [[ $choice == "n" || $choice == "N" ]]; then
+      return
+    else
+      clear
+      echo -e "${red}Invalid input. Try again${reset}\n"
+    fi
+  done
+}
+
 # check if there's more than one device and ask which one the user wants
 get_device_choice() {
   valid_device_choice=false
@@ -45,6 +115,8 @@ get_device_choice() {
 
   if [ "$device_count" -eq "0" ]; then
     echo -e "${red}No devices found.${reset}"
+    
+    device_not_found
   elif [ "$device_count" -eq "1" ]; then
     selected_device=${devices[0]}
     device_exists=true
